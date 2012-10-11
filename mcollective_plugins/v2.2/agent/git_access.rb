af1c88a6dd67a099022a9a1f6a03070366b607e0
Rich Pelavin <rich.pelavin@gmail.com>
2012-10-11 19:09:36 +0000

    refining mcolelctive agent git_access

--- mcollective_plugins/v2.2/agent/git_access.rb
+++ mcollective_plugins/v2.2/agent/git_access.rb
@@ -4,15 +4,6 @@ module MCollective
       action "add_rsa_info" do 
         SSH_FOLDER_PATH = '/root/.ssh'
 
-        def validate_existance(file)
-          raise "Missing #{file} unable to continue." unless File.exists?(file)
-        end
-
-        def append_to_file(file,value)
-          outfile = File.open(file,'a')
-          outfile.print value
-          outfile.close
-        end
 
         def validate_request(req)
           required_params = [:agent_ssh_key_public, :agent_ssh_key_private, :server_ssh_key_public, :server_hostname ]
@@ -31,22 +22,22 @@ module MCollective
           rsa_pub_path = "#{SSH_FOLDER_PATH}/id_rsa.pub"
           known_hosts  = "#{SSH_FOLDER_PATH}/known_hosts"
 
-          # validate existance of needed files
-          validate_existance(rsa_path)
-          validate_existance(rsa_pub_path)
+          # fails if these files already exists
+          if File.exists?(rsa_path)
+            raise "File #{rsa_path} already exists"
+          end
+          if File.exists?(rsa_pub_path)
+            raise "File #{rsa_pub_path} already exists"
+          end
 
           # validate request
           validate_request(request)
 
-          # create files if not present
-          unless File.exists?(known_hosts)
-            FileUtils.touch known_hosts
-            raise "Not able to create hosts file #{known_hosts}, unable to continue" unless File.exists?(known_hosts)
-          end
-
-          append_to_file(rsa_path,request[:agent_ssh_key_private])
-          append_to_file(rsa_pub_path, request[:agent_ssh_key_public])
-          append_to_file(known_hosts,"#{request[:server_hostname]} ssh-rsa #{request[:server_ssh_key_public]}")
+          # create files 
+          File.open(rsa_path,"w",0600){|f|f.print request[:agent_ssh_key_private]}
+          File.open(rsa_pub_path,"w"){|f|f.print request[:agent_ssh_key_public]}
+          #create or append
+          File.open(known_hosts,"a"){|f|f.print "#{request[:server_hostname]} ssh-rsa #{request[:server_ssh_key_public]}"}
           
           reply.data   = { :status => 'OK', :message => "Public/Private keys added successfully."}
         rescue Exception => e
