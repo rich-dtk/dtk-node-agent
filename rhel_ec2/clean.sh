fa9037ab1f37c3444146237aadaef196cbcd2c29
rich Pelavin <rich.pelavin@gmail.com>
2012-10-25 13:10:56 -0400

    updated clean.sh for rhel

--- rhel_ec2/clean.sh
+++ rhel_ec2/clean.sh
@@ -3,9 +3,8 @@
 /etc/init.d/mcollective stop
 base_dir=`dirname "$0"`
 
-echo "temporarily commented out update to facts.yaml and server.cfg to keep server addr nailed"
-#cat ${base_dir}/clean/etc/mcollective/facts.yaml > /etc/mcollective/facts.yaml
-#cat ${base_dir}/clean/etc/mcollective/server.cfg > /etc/mcollective/server.cfg
+cat ${base_dir}/clean/etc/mcollective/facts.yaml > /etc/mcollective/facts.yaml
+cat ${base_dir}/clean/etc/mcollective/server.cfg > /etc/mcollective/server.cfg
 
 rm -r /etc/puppet/modules/*
 cp -r ${base_dir}/../src/etc/puppet/modules/r8 /etc/puppet/modules/
