710393cdc7d4cdf64828861dff32457ff62ca9ed
Aldin Milanovic <aldin@atlantbh.com>
2012-11-07 16:44:31 +0100

    Implemented one functional part of DTK-359 Ability for tail to take a grep expression

--- mcollective_plugins/v2.2/agent/tail.rb
+++ mcollective_plugins/v2.2/agent/tail.rb
@@ -19,8 +19,12 @@ module MCollective
           # if there is start line from CLI request we use it, if not we take last BATCH_SIZE_OF_LOG lines
           start_line = (request[:start_line].empty? ? last_line-BATCH_SIZE_OF_LOG : request[:start_line])
           # returns needed lines
-          output = `tail -n +#{start_line} #{request[:log_path]}`
-          
+          if (request[:grep_option].empty? || request[:grep_option].nil?)
+            output = `tail -n +#{start_line} #{request[:log_path]}`
+          else
+            output = `tail -n +#{start_line} #{request[:log_path]} | grep #{request[:grep_option]}`
+          end
+
           reply[:data]      = { :output => output, :last_line => last_line }
           reply[:pbuilderid] = Facts["pbuilderid"]
           reply[:status]    = :ok
