e30ee94cb12abb002863a9f5d0d442cf0bb0f695
Aldin Milanovic <aldin@atlantbh.com>
2012-11-19 15:04:41 +0100

    DTK-386 Tail node agent computes a size that is negative

--- ubuntu_ec2/usr/share/mcollective/plugins/mcollective/agent/tail.rb
+++ ubuntu_ec2/usr/share/mcollective/plugins/mcollective/agent/tail.rb
@@ -17,7 +17,13 @@ module MCollective
           # returns total number of lines in file, one is to start next iteration with new line
           last_line  = `wc -l #{request[:log_path]} | awk '{print $1}'`.to_i + 1
           # if there is start line from CLI request we use it, if not we take last BATCH_SIZE_OF_LOG lines
-          start_line = (request[:start_line].empty? ? last_line-BATCH_SIZE_OF_LOG : request[:start_line])
+          if request[:start_line].empty?
+            # If BATCH_SIZE_OF_LOG is bigger than last_line, then start line will be 0
+            start_line = (last_line > BATCH_SIZE_OF_LOG) ? last_line-BATCH_SIZE_OF_LOG : 0
+          else
+            start_line = request[:start_line]
+          end
+          
           # returns needed lines
           if (request[:grep_option].empty? || request[:grep_option].nil?)
             output = `tail -n +#{start_line} #{request[:log_path]}`
