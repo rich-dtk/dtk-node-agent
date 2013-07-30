8d413c81688614cbefc66301846076edca6e6ff8
Rich Pelavin <rich@reactor8.com>
2013-07-30 01:06:06 +0000

    experiment with dev manager that doe snot need to restart

--- mcollective_additions/plugins/v2.2/agent/dev_manager.rb
+++ mcollective_additions/plugins/v2.2/agent/dev_manager.rb
@@ -8,35 +8,43 @@ module MCollective
 
       action "inject_agent" do
         begin
+          #TODO: look at locking out other agents calls during this segment
+          ret = Response.new() 
 
-          ret ||= Response.new() 
-
-          request[:agent_files].each do |k,v|
-            if v == :deleted
-              File.delete("#{AGENT_MCOLLECTIVE_LOCATION}#{k}")
-              next
-            end
-            content = Base64.decode64(v)
-            File.open("#{AGENT_MCOLLECTIVE_LOCATION}#{k}",'w') do |file|
-              file << content
+          #first update the agent files and tell mcolelctive about it
+          request[:agent_files].each do |agent_path,v|
+            agent_name = agent_path.gsub(/\.rb$/,"") 
+            full_path = "#{AGENT_MCOLLECTIVE_LOCATION}#{agent_path}"
+            if v.kind_of?(Symbol) and v == :deleted
+              MCHelper.uninstall(agent_name)
+              File.delete(full_path) if File.exists?(full_path)
+            else
+              content = Base64.decode64(v)
+              File.open(full_path,'w'){|file|file << content}
+              MCHelper.install_or_reinstall_agent(agent_name)
             end
           end
           ret.set_status_succeeded!()
-
-          t1 = Thread.new do 
-            sleep(2)
-            system("sudo /etc/init.d/mcollective restart")
-          end
-
-          return ret
-
         rescue Exception => e
           Log.error e
           ret.set_status_failed!()
           error_info = { :error => { :message => "Error syncing agents: #{e}" } }
           ret.merge!(error_info)
         end
-        return ret
+        ret
+      end
+
+      module MCHelper
+        def self.uninstall_agent(agent_name)
+          ::MCollective::PluginManager.delete("#{agent_name}_agent")
+        end
+        def self.install_or_reinstall_agent(agent_name)
+          #Modified fragment fRom https://github.com/ripienaar/mcollective-agent-debugger
+          classname = "MCollective::Agent::#{agent_name.capitalize}"
+          ::MCollective::PluginManager.delete("#{agent_name}_agent")
+          ::MCollective::PluginManager.loadclass(classname)
+          ::MCollective::PluginManager << {:type => "#{agent_name}_agent", :class => classname}
+        end
       end
     end
     #TODO: this should be common accross Agents
@@ -51,19 +59,13 @@ module MCollective
         Hash.new.merge(self)
       end
 
-      def failed?()
-        self[:status] == :failed
-      end
-
       def set_status_failed!()
         self[:status] = :failed
       end
       def set_status_succeeded!()
         self[:status] = :succeeded
       end
-      def set_dynamic_attributes!(dynamic_attributes)
-        self[:dynamic_attributes] = dynamic_attributes
-      end
     end
   end
 end
+ 
