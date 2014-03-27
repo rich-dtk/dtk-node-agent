23cc34075d07291850896591ee735f1d7b44e4f0
Haris KRAJINA <haris@atlantbh.com>
2014-03-27 16:05:23 +0100

    Added global initialization which was  creating problems for us

--- mcollective_additions/plugins/v2.2/util/puppetrunner.rb
+++ mcollective_additions/plugins/v2.2/util/puppetrunner.rb
@@ -5,6 +5,9 @@ module MCollective
     class PuppetRunner
 
       def self.apply(puppet_definition, resource_hash)
+        Puppet.settings.initialize_global_settings
+        Puppet.settings.initialize_app_defaults(Puppet::Settings.app_defaults_for_run_mode(Puppet.run_mode))
+
         pup = Puppet::Type.type(puppet_definition).new(resource_hash)
         catalog = Puppet::Resource::Catalog.new
         catalog.add_resource pup
