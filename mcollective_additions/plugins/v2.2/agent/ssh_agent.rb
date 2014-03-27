c326878868090f5e05a8b72cbeb1aa253876a165
Haris KRAJINA <haris@atlantbh.com>
2014-03-27 17:32:06 +0100

    Added puppet fixes for SSH

--- mcollective_additions/plugins/v2.2/agent/ssh_agent.rb
+++ mcollective_additions/plugins/v2.2/agent/ssh_agent.rb
@@ -7,25 +7,33 @@ module MCollective
       AGENT_MCOLLECTIVE_LOCATION = "#{::MCollective::Config.instance.libdir}/mcollective/agent/"
       SSH_AUTH_KEYS_FILE_NAME    = "authorized_keys"
 
-
       action "grant_access" do
         validate :rsa_pub_key, String
         validate :rsa_pub_name, String
         validate :system_user, String
 
         if does_user_exist?(request[:system_user])
-          begin 
-            ::MCollective::Util.loadclass("MCollective::Util::PuppetRunner")
-            ::MCollective::Util::PuppetRunner.apply(
-              :ssh_authorized_key,
-              {
+          begin
+            puppet_params = {
                 :name => request[:rsa_pub_name],
                 :ensure => 'present',
                 :key =>normalize_rsa_pub_key(request[:rsa_pub_key]),
                 :type => 'ssh-rsa',
                 :user => request[:system_user]
               }
-            )
+
+            ::MCollective::Util.loadclass("MCollective::Util::PuppetRunner")
+            ::MCollective::Util::PuppetRunner.apply(:ssh_authorized_key, puppet_params)
+
+            # There is a bug where we are expiriencing issues with above changes not taking effect for no apperent reason
+            # if detected we repeat puppet apply
+
+            unless key_added?(puppet_params[:user], puppet_params[:key])
+              Log.info("Fallback, repeating SSH access grant")
+              ::MCollective::Util::PuppetRunner.apply(:ssh_authorized_key, puppet_params)
+            end
+
+            raise "We were not able to add SSH access for given node (PuppetError)" unless key_added?(puppet_params[:user], puppet_params[:key])
 
             reply[:data] = { :message => "Access to system user '#{request[:system_user]}' has been granted for '#{request[:rsa_pub_name]}'"}
           rescue Exception => e
@@ -37,7 +45,6 @@ module MCollective
         reply[:pbuilderid] = Facts["pbuilderid"]
         reply[:status] = :ok
       end
-
 
       action "revoke_access" do
         validate :rsa_pub_name, String
@@ -69,6 +76,11 @@ module MCollective
 
       def does_user_exist?(system_user)
         !File.open('/etc/passwd').grep(/home\/#{system_user}:/).empty?
+      end
+
+      def key_added?(system_user, pub_key)
+        results = `more /home/#{system_user}/.ssh/#{SSH_AUTH_KEYS_FILE_NAME} | grep #{pub_key}`
+        !results.empty?
       end
 
       def normalize_rsa_pub_key(rsa_pub_key)
--- mcollective_additions/plugins/v2.2/util/puppetrunner.rb
+++ mcollective_additions/plugins/v2.2/util/puppetrunner.rb
@@ -8,13 +8,24 @@ module MCollective
         Puppet.settings.initialize_global_settings
         Puppet.settings.initialize_app_defaults(Puppet::Settings.app_defaults_for_run_mode(Puppet.run_mode))
 
+        Log.info("Puppet Runner, INPUT :")
+        Log.info(puppet_definition)
+        Log.info(resource_hash.inspect)
+        Log.info("########################################################################")
+
         pup = Puppet::Type.type(puppet_definition).new(resource_hash)
         catalog = Puppet::Resource::Catalog.new
         catalog.add_resource pup
-        catalog.apply
+        catalog.apply()
+
+        Log.info("Puppet Runner, OUTPUT: ")
+        Log.info(Thread.current[:report_status])
+        Log.info(Thread.current[:report_info])
+        Log.info("########################################################################")
         true
       end
 
     end
   end
 end
+
