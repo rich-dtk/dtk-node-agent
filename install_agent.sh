959840e49970b8251792ee974078dce3cb9a5f52
dduvnjak <dario.duvnjak@gmail.com>
2014-10-23 16:24:53 +0200

    DTK-1714: Fix errors when doing sanitize

--- install_agent.sh
+++ install_agent.sh
@@ -96,6 +96,6 @@ args="$*"
 
 if [[ ${args} =~ "--sanitize" ]]; then
   # sanitize the AMI (creating unique ssh host keys will be handled by the cloud-init package)
-  find /root/.*history /home/*/.*history -exec rm -f {} \;
+  find /root /home -name ".*history" -exec rm -f {} \;
   find /root /home /etc -name "authorized_keys" -exec rm -f {} \;
 fi;
