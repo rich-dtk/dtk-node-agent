c3e335d6d9841790d7591734ec60d46bc59a91f1
Haris Krajina <haris@atlantbh.com>
2015-03-09 13:08:00 +0100

    Added check before initializing puppet specific methods

--- mcollective_additions/plugins/v2.2/util/puppetrunner.rb
+++ mcollective_additions/plugins/v2.2/util/puppetrunner.rb
@@ -5,8 +5,13 @@ module MCollective
     class PuppetRunner
 
       def self.apply(puppet_definition, resource_hash)
-        Puppet.settings.initialize_global_settings
-        Puppet.settings.initialize_app_defaults(Puppet::Settings.app_defaults_for_run_mode(Puppet.run_mode))
+        if Puppet.settings.respond_to?(:initialize_global_settings)
+          Puppet.settings.initialize_global_settings
+        end
+
+        if Puppet.settings.respond_to?(:initialize_app_defaults)
+          Puppet.settings.initialize_app_defaults(Puppet::Settings.app_defaults_for_run_mode(Puppet.run_mode))
+        end
 
         Log.info("Puppet Runner, INPUT :")
         Log.info(puppet_definition)
