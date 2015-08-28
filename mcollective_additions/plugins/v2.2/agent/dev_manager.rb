55847dde609d8ae8b42b05b249578b9eeb88f164
Haris Krajina <haris@atlantbh.com>
2015-08-28 12:58:21 +0200

    Fixed a bug for dev_manger dtk-action-agent sync

--- mcollective_additions/plugins/v2.2/agent/dev_manager.rb
+++ mcollective_additions/plugins/v2.2/agent/dev_manager.rb
@@ -1,6 +1,7 @@
 require 'base64'
 require 'grit'
 require 'tmpdir'
+require 'fileutils'
 
 module MCollective
   module Agent
@@ -34,20 +35,24 @@ module MCollective
           dtk_aa_branch = request[:action_agent_branch]
 
           # DTK Action Agent Sync
-          Log.instance.info("Started DTK Action Agent sync, temp dir '#{DTK_AA_TEMP_DIR}'")
-          cmd_opts = { :timeout => 60 }
+          begin
+            Log.instance.info("Started DTK Action Agent sync, temp dir '#{DTK_AA_TEMP_DIR}'")
+            cmd_opts = { :timeout => 60 }
 
-          clone_args = [dtk_aa_url, DTK_AA_TEMP_DIR]
-          clone_args += ["-b", dtk_aa_branch]
-          ::Grit::Git.new('').clone(cmd_opts, *clone_args)
-          Log.instance.info("Cloned latest code from branch '#{dtk_aa_branch}' for DTK Action Agent.")
+            clone_args = [dtk_aa_url, DTK_AA_TEMP_DIR]
+            clone_args += ["-b", dtk_aa_branch]
+            ::Grit::Git.new('').clone(cmd_opts, *clone_args)
+            Log.instance.info("Cloned latest code from branch '#{dtk_aa_branch}' for DTK Action Agent.")
 
-          output = `cd #{DTK_AA_TEMP_DIR} && gem build dtk-action-agent.gemspec && gem install dtk-action-agent-*.gem --no-ri --no-rdoc`
-          result = $?
-          if result.success?
-            Log.instance.info("DTK Action Agent has been successfully update from branch '#{dtk_aa_branch}'")
-          else
-            Log.instance.error("DTK Action Agent could not be updated, reason: #{output}")
+            output = `cd #{DTK_AA_TEMP_DIR} && /opt/puppet-omnibus/embedded/bin/gem build #{DTK_AA_TEMP_DIR}/dtk-action-agent.gemspec && /opt/puppet-omnibus/embedded/bin/gem install #{DTK_AA_TEMP_DIR}/dtk-action-agent-*.gem --no-ri --no-rdoc`
+            result = $?
+            if result.success?
+              Log.instance.info("DTK Action Agent has been successfully update from branch '#{dtk_aa_branch}'")
+            else
+              Log.instance.error("DTK Action Agent could not be updated, reason: #{output}")
+            end
+          ensure
+            FileUtils.rm_rf DTK_AA_TEMP_DIR
           end
 
           t1 = Thread.new do
