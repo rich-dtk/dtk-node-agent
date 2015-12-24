6eba11f1bb92f386e33ec13314106b877d021aff
Haris Krajina <haris@atlantbh.com>
2015-12-24 16:06:09 +0100

    Added serialization to tmp foler so that info can be shared with arbiter

--- puppet_additions/modules/r8/manifests/export_variable.rb
+++ puppet_additions/modules/r8/manifests/export_variable.rb
@@ -1,10 +1,11 @@
-define "r8::export_variable", :content => Puppet::Parser::AST::Leaf::Undef.new({:value => '***'}) do 
+define "r8::export_variable", :content => Puppet::Parser::AST::Leaf::Undef.new({:value => '***'}) do
   if @name =~ /(^.+)::(.+$)/
     component = $1
     attribute = $2
-    if content = (@content == '***' ? scope.lookupvar(@name) : @content) 
+    if content = (@content == '***' ? scope.lookupvar(@name) : @content)
       p = Thread.current[:exported_variables] ||= Hash.new
-      (p[component] ||= Hash.new)[attribute] = content 
+      (p[component] ||= Hash.new)[attribute] = content
+      File.open('/tmp/dtk_exported_variables', 'w') { |f| f.write(Marshal.dump(p)) }
     end
   end
-end 
+end
