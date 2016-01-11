0083a6cc9f03387302571da61af864f406809945
Haris Krajina <haris@atlantbh.com>
2016-01-11 13:34:38 +0100

    Added support for exported_files'

--- puppet_additions/modules/r8/lib/puppet/type/r8_export_file.rb
+++ puppet_additions/modules/r8/lib/puppet/type/r8_export_file.rb
@@ -36,13 +36,14 @@ module Puppet
         if resource[:name] =~ /(^.+)\.(.+$)/
           cmp_name = $1
           attr_name = $2
-          
+
           cmp_ref = cmp_name.gsub(/[.]/,"::")
           if def_key =  resource[:definition_key]
             cmp_ref = "#{cmp_ref}[#{def_key}]"
           end
           p = (Thread.current[:exported_files] ||= Hash.new)[cmp_ref] ||= Hash.new
           p[attr_name] = filename
+          File.open('/tmp/dtk_exported_files', 'w') { |f| f.write(Marshal.dump(p)) }
         else
           raise Puppet::Error, "ill-formed component with name (#{resource[:name]})"
         end
